4370841b0d8c0629c30afecd4cb394d77ec9c862
Barry Li <bli031@uottawa.ca>
2019-03-27 00:21:21 -0400

    plotly edit

--- src/index.js
+++ src/index.js
@@ -27,6 +27,50 @@ var data = [trace1,trace2];
 
 var layout = {
   title: 'USD vs CAD, USD vs CNY',
+  xaxis: {
+    autorange: true,
+    range: ['1981-06-10', '2016-11-30'],
+    rangeselector: {buttons: [
+        {
+          count: 1,
+          label: '1y',
+          step: 'year',
+          stepmode: 'backward'
+        },
+        {
+          count: 5,
+          label: '5y',
+          step: 'year',
+          stepmode: 'backward'
+        },
+        {
+          count: 10,
+          label: '10y',
+          step: 'year',
+          stepmode: 'backward'
+        },
+        {
+          count: 20,
+          label: '20y',
+          step: 'year',
+          stepmode: 'backward'
+        },
+        {
+          count: 30,
+          label: '30y',
+          step: 'year',
+          stepmode: 'backward'
+        },
+        {step: 'all'}
+      ]},
+    rangeslider: {range: ['1981-06-10', '2016-11-30']},
+    type: 'date'
+  },
+  yaxis: {
+    autorange: true,
+    range: [0, 10],
+    type: 'linear'
+  }
 };
 
 plotlyDiv = document.getElementById('plotly-example');
